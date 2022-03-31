fe03b209e638b241aac015d5fd93f371a1f9283a
unknown <spnaveen675@gmail.com>
2022-03-31 23:42:12 +0530

    Added Promises practice File

--- ASync/Promises/practice1.js
+++ ASync/Promises/practice1.js
@@ -1,49 +1,70 @@
-let promise1 = a => {
+// let promise1 = a => {
+//     return new Promise((resolve,reject)=> {
+
+//     if(! a){
+//         reject("Failed");
+//     }
+//     else{
+//         resolve("Success");
+//     }
+
+// })
+// }
+
+
+// let success = result => {
+//     console.log(result);
+// }
+// let fail = error => {
+//     console.log(error);
+// }
+
+// let promise2 = new Promise((resolve,reject)=> {
+
+//     fetch("https://api.github.com/users/naveen675/repos").then(response => {
+
+//         if(! response.ok){
+//             reject(new Error(`Not able to fetch Data ${response.status}`));
+//         }
+//         else{
+//             resolve(response.json());
+//         }
+//     })
+    
+// })
+
+// success = result => {
+//     console.log(result);
+// }
+// fail = error => {
+//     console.log(error);
+// }
+
+// Promise.all([promise1(true),promise2]).then(results => {
+//     console.log(results);
+// });
+
+// console.log(promise1(false));
+
+
+let timeout = delay => {
+
     return new Promise((resolve,reject)=> {
 
-    if(! a){
-        reject("Failed");
-    }
-    else{
-        resolve("Success");
-    }
-
+        setTimeout(resolve,delay);
+        
 })
 }
 
-
-let success = result => {
-    console.log(result);
-}
-let fail = error => {
-    console.log(error);
-}
-
-let promise2 = new Promise((resolve,reject)=> {
-
-    fetch("https://api.github.com/users/naveen675/repos").then(response => {
-
-        if(! response.ok){
-            reject(new Error(`Not able to fetch Data ${response.status}`));
-        }
-        else{
-            resolve(response.json());
-        }
-    })
-    
-})
-
-success = result => {
-    console.log(result);
-}
-fail = error => {
-    console.log(error);
-}
-
-Promise.all([promise1(true),promise2]).then(results => {
-    console.log(results);
+let delay = 3000;
+timeout(delay).then(response => {
+    console.log(delay);
 });
-
-console.log(promise1(false));
-
-
+delay = 1000;
+timeout(delay).then(response => {
+    console.log(delay);
+});
+delay = 2000;
+timeout(delay).then(response => {
+    console.log(delay);
+});
