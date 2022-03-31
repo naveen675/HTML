76ff458a1af591b958a7dfdd3a482236a65e4d6c
unknown <spnaveen675@gmail.com>
2022-03-31 23:20:46 +0530

    Added Promises practice File

---
+++ ASync/Promises/practice1.js
@@ -0,0 +1,49 @@
+let promise1 = a => {
+    return new Promise((resolve,reject)=> {
+
+    if(! a){
+        reject("Failed");
+    }
+    else{
+        resolve("Success");
+    }
+
+})
+}
+
+
+let success = result => {
+    console.log(result);
+}
+let fail = error => {
+    console.log(error);
+}
+
+let promise2 = new Promise((resolve,reject)=> {
+
+    fetch("https://api.github.com/users/naveen675/repos").then(response => {
+
+        if(! response.ok){
+            reject(new Error(`Not able to fetch Data ${response.status}`));
+        }
+        else{
+            resolve(response.json());
+        }
+    })
+    
+})
+
+success = result => {
+    console.log(result);
+}
+fail = error => {
+    console.log(error);
+}
+
+Promise.all([promise1(true),promise2]).then(results => {
+    console.log(results);
+});
+
+console.log(promise1(false));
+
+
